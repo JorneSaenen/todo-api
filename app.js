54d85d4729091f7ec3a61753c728fb64aecbdfd1
Jorne Saenen <59235736+JorneSaenen@users.noreply.github.com>
2021-06-18 12:30:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/api/todos');
 
 const app = express();
-app.options('*', cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
